1165087285111bb08af4ab95a5241a2a38a2a519
it <it@DESKTOP-JKE555E>
2019-07-25 12:38:59 +0900

    commit test

---
+++ javaProject/src/javaProject/MainTest.java
@@ -0,0 +1,19 @@
+package javaProject;
+
+public class MainTest {
+	private String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+}
